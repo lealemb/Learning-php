f3c58535af0616bbac032f817522cba55b92f39c
lealemb <lealemb84@gmail.com>
2024-12-21 03:16:11 +0300

    variables and datatypes

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$name = "John";
+$food = "Pizza";
+$color = "Blue";
+
+$age = 25;
+$email = "fake@gmail.com";
+$phone = "123-456-7890";
+
+$gpa = 3.5;
+
+echo "Hello {$name} <br>";
+echo "you like {$food}";
+echo "your favorite color is {$color} <br>";
+
+echo "you are {$age} years old <br>";
+
+echo "your email is {$email} <br>";
+echo "your gpa is {$gpa} <br>";
+echo "your phone number is {$phone}";
