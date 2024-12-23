26f1038136169ab3ebdf54429b2d8d1cd60c1630
lealemb <lealemb84@gmail.com>
2024-12-24 01:40:11 +0300

    session

---
+++ session/login.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="login.php" method="post">
+        username:<br>
+        <input type="text" name="username"><br>
+        password:<br>
+        <input type="password" name="password"><br>
+        <input type="submit" name="login" value="login">
+    </form>
+</body>
+
+</html>
+
+<?php
+if (isset($_POST['login'])) {
+
+    if (!empty($_POST["username"]) && !empty($_POST["password"])) {
+        $_SESSION['username'] = $_POST['username'];
+        $_SESSION['password'] = $_POST['password'];
+
+        header("location:home.php"); // This is goes to home page
+    } else {
+        echo "missing username or password";
+    }
+}
+
+
+?>
